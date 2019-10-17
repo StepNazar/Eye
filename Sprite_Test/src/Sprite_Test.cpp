ab4815cbdc0de4bfa3fc47448a40474469a170f5
NS <NS@26.112.95.233>
2019-10-17 20:45:05 +0300

    Sprite tested

--- Sprite_Test/src/Sprite_Test.cpp
+++ Sprite_Test/src/Sprite_Test.cpp
@@ -15,24 +15,17 @@ using namespace std;
 
 int main()
 {
-	Coordinates x = 0;
-	Coordinates y = 5;
-
     sf::RenderWindow window(sf::VideoMode(920, 580), "SFML works!", sf::Style::Default);
-    sf::RectangleShape rectangle(sf::Vector2f(920.f, 35.f));
-    sf::CircleShape circle(35.f);
-    sf::Image image;
-    circle.move(50, 50);
-    rectangle.setSize(sf::Vector2f(920.f, 35.f));
-    sf::RectangleShape rectangle1[11];
 
-    for (int i = 0; i < 11; i++){
-        rectangle1[i].setSize(sf::Vector2f(25.f, 25.f));
-        rectangle1[i].move(x, y);
-        x += 40;
-        rectangle1[i].setOutlineThickness(3.f);
-        rectangle1[i].setOutlineColor(sf::Color(0, 0, 0));
-    }
+    // Sprite customization
+    sf::Sprite sprite;
+    sf::Texture texture;
+
+    texture.create(200, 200);
+    texture.loadFromFile("image.png");
+
+    sprite.setTexture(texture);
+    sprite.setColor(sf::Color(200, 20, 30));
 
     while (window.isOpen())
     {
@@ -44,12 +37,8 @@ int main()
         }
 
         window.clear();
-        window.draw(rectangle);
-		window.draw(circle);
-        for (int i = 0; i < 11; i++){
-            window.draw(rectangle1[i]);
-        }
 
+        window.draw(sprite);
         window.display();
     }
 
